d107c3d6219b44d3ec4671733b7d12ef5dd4eaa1
giopane98 <127940277+giopane98@users.noreply.github.com>
2023-05-10 11:32:54 +0200

    first version portfolio

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import EnjoyButton from './components/Enjoy-button.vue';
+import Gallery from './components/Gallery.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="Ciao a tutti!" />
+    </div>
+  </header>
+
+  <main>
+    <Gallery 
+    title="Prima"
+    abstract="Questa sarà la mia prima foto"
+    imageUrl="/images/neom-4q87Z96NBYg-unsplash.jpg"
+    />
+    <Gallery 
+    title="Seconda" 
+    abstract="Questa sarà la mia seconda foto"
+    imageUrl="/images/neom-dMSYsXdwl9M-unsplash.jpg"
+    />
+    <Gallery 
+    title="Terza" 
+    abstract="Questa sarà la mia terza foto"
+    imageUrl="/images/neom-epQdtCvBrc4-unsplash.jpg"
+    />
+    <EnjoyButton />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,53 @@
+<script setup> 
+/* defineProps = che il mio progetto vue avrà delle proprietà che cambieranno */
+defineProps({
+  title: String,
+  abstract: String,
+  imageUrl: String,
+})
+</script>
+
+<template>
+    <div class="section">
+      <div class="image">
+         <img :src="imageUrl" width="125" height="125" />
+      </div>
+      <div class="video-text">
+        <h1>{{ title }}</h1>
+        <p class="description">{{ abstract }}</p>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+img {
+  display: block;
+  border-radius: 100%;
+}
+
+.section {
+  background-color: #F2F2FF;
+  border-radius: 5px;
+  margin-bottom: 1.5rem;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+}
+
+h1 {
+  font-size: 1.4rem;
+  color: #9898FF;
+  font-weight: 800;
+}
+.description {
+  font-size: 1.2rem;
+  font-weight: 300;
+  color: #7878ff;
+  margin-top: -5px;
+}
+
+.video-text{
+  margin-left: 2rem;
+}
+</style>
